bf55c8ca624f8ac8926b68333a5892bbfeafd237
CrisL1990 <lucchetta.cris@gmail.com>
2022-02-17 15:30:50 +0100

    prove 2

--- main.js
+++ main.js
@@ -11,18 +11,18 @@ const app = new Vue({
 
     mounted() {
         //this.apiRequest(this.iterations);
-
+        /*
         for(let i = 0; i < this.iterations; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 
             .then((element) => {
                 this.mailList.push(element.data.response);
             })
-        } 
+        }*/ 
     },
 
     methods:{
-        /*
+        
         apiRequest: function(iterations){
 
             for(let i = 0; i < iterations; i++){
@@ -33,6 +33,8 @@ const app = new Vue({
                 })
             } 
         }
-        */
+        
+
+
     }
 })
